9f23c5f7335c48cf7ec2edb261cae3b20f7e292d
danieldanielito <113646615+danieldanielito@users.noreply.github.com>
2023-02-14 17:44:28 -0300

    throw

--- src/java_pila_ejecucion/Flujo.java
+++ src/java_pila_ejecucion/Flujo.java
@@ -15,24 +15,8 @@ public class Flujo {
 	}
     public static void metodo2() {
     	System.out.println("inicio metodo2");
-    	for (int i = 1; i < 6; i++) {
-    		System.out.println(i);
-    		try{
-    			if(i==3) {
-    				int res = i/0;
-            		System.out.println(res);	
-    			}
-    			
-        		String cad = null;
-        		System.out.println(cad.toString());
-    			
-    		}catch(ArithmeticException | NullPointerException e) {
-    			System.out.println("atrapa excepcion");
-    			System.out.println(e.getMessage());
-    			e.printStackTrace();
-    		
-    		}	
-		}	
-		System.out.println("fin metodo2");
+    	
+    	throw new ArithmeticException("surgio un error");
+		//System.out.println("fin metodo2");
 	}
 }
