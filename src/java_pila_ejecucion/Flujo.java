20a35cf8aee26cfa12e10c6fda58f901d5c62664
danieldanielito <113646615+danieldanielito@users.noreply.github.com>
2023-02-15 16:29:00 -0300

    excepcion

--- src/java_pila_ejecucion/Flujo.java
+++ src/java_pila_ejecucion/Flujo.java
@@ -3,20 +3,25 @@ package java_pila_ejecucion;
 
 
 public class Flujo {
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		System.out.println("inicio main");
 		metodo1();
 		System.out.println("fin main");
 	}
 	public static void metodo1() {
 		System.out.println("inicio metodo1");
+		try {
 		metodo2();
+		}catch(MiExcepcion me) {
+			System.out.println("mi excepcion");
+			me.printStackTrace();
+		}
 		System.out.println("fin metodo1");
 	}
-    public static void metodo2() {
+    public static void metodo2() throws MiExcepcion {
     	System.out.println("inicio metodo2");
     	
-    	throw new ArithmeticException("surgio un error");
+    	throw new MiExcepcion("mi excepcion fue lanzada");
 		//System.out.println("fin metodo2");
 	}
 }
---
+++ src/java_pila_ejecucion/MiExcepcion.java
@@ -0,0 +1,12 @@
+package java_pila_ejecucion;
+
+public class MiExcepcion extends Exception {
+	
+	public MiExcepcion() {
+		super();
+	}
+    
+	public MiExcepcion(String message) {
+		super(message);
+	}
+}
